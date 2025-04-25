7a20647ec518be95268afc06859dd6e840b1dfe9
christianantonsson <b22chran@student.his.se>
2025-04-25 13:47:12 +0200

    removed old code for the get-function that didn't include the convertion of the result to json

--- weather-backend/server.js
+++ weather-backend/server.js
@@ -15,21 +15,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
-
-// GET /ndrbrd?landskap=xxx&station=yyy
-// app.get('/weather', async (req, res) => {
-//   const { landskap, station } = req.query;
-//   let query = {};
-  
-//   if (landskap) query.Landskap = new RegExp(landskap, 'i');
-//   if (station) query.Station = new RegExp(station, 'i');
-
-//   try {
-//     const data = await Weather.find(query).limit(10000);
-//   } catch (err) {
-//     res.status(500).json({ error: 'Failed to fetch data' });
-//   }
-// });
 app.get('/weather', async (req, res) => {
   const { landskap, station } = req.query;
   let query = {};
